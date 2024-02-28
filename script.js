14e6c95cd36ee39dc734f307fe1034aff36debff
iamfrdfa <fri.faraji@gmail.com>
2024-02-28 16:52:39 +0100

    deleted bootstrap, added image and styled the pokecard

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const AllPOKEMONS_API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
 const POKEMON_NAME = [];
 let currentPokemon;
+let start = 0;
 
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let url = 'https://pokeapi.co/api/v2/pokemon/bulbasaur';
     let response = await fetch(url);
     currentPokemon = await response.json();
     
@@ -14,5 +15,13 @@ async function loadPokemon() {
 
 function renderPokemonInfo() {
     document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
-    document.getElementById('pokemonImage').src = currentPokemon['sprites']['front_shiny'];
+    document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['home']['front_default'];
+}
+
+function openPokemonInfo() {
+
+}
+
+function init() {
+    loadPokemon();
 }
